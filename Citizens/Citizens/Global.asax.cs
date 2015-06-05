4596355b80b40d6388cd9498d3bbf98f55538620
Vitaliy Guz <vitality2007@mail.ru>
2015-06-05 12:10:24 +0000

    CORS for Chrome Imported from Changeset 175

--- Citizens/Citizens/Global.asax.cs
+++ Citizens/Citizens/Global.asax.cs
@@ -22,13 +22,22 @@ namespace Citizens
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            GlobalFilters.Filters.Add(new CustomAuthorize());//GlobalFilters.Filters.Add(new AuthorizeAttribute() { Roles = "Admin, SuperUser" });
-            GlobalConfiguration.Configuration.MessageHandlers.Add(new HttpMessageHandler(new CorsHandler(
-            "http://localhost:36561",
-            "PUT,GET,PATCH,DELETE,HEAD,OPTIONS",
-            "Content-Type",
-            true
-    )));
+            GlobalFilters.Filters.Add(new CustomAuthorize());//GlobalFilters.Filters.Add(new AuthorizeAttribute() { Roles = "Admin, SuperUser" });            
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                //These headers are handling the "pre-flight" OPTIONS call sent by the browser
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+
         }
     }
 
@@ -51,95 +60,5 @@ namespace Citizens
         }
     }
 
-    public abstract class Handler
-    {
-        public string Methods { get; protected set; }
-        public abstract HttpResponseMessage Apply(Task<HttpResponseMessage> response, HttpRequestMessage request);
-    }
 
-    public class CorsHandler : Handler
-    {
-        private readonly bool _corsEnabled;
-        private readonly string _allowedOrigins;
-        private readonly string _allowedHeaders;
-        private readonly bool _allowCredentials;
-
-        public CorsHandler(string allowedOrigins,
-                            string allowedMethods,
-                            string allowedHeaders,
-                            bool allowCredentials)
-        {
-            // Force to be explicit on cors configuration
-            if (string.IsNullOrEmpty(allowedOrigins) ||
-                string.IsNullOrEmpty(allowedMethods) ||
-                string.IsNullOrEmpty(allowedHeaders))
-            {
-                return;
-            }
-
-            _corsEnabled = true;
-            _allowedOrigins = allowedOrigins;
-            _allowedHeaders = allowedHeaders;
-            _allowCredentials = allowCredentials;
-            Methods = allowedMethods;
-        }
-
-        public override HttpResponseMessage Apply(Task<HttpResponseMessage> prior, HttpRequestMessage request)
-        {
-            var response = prior.Result;
-
-            // Prevent the "found" redirect and return 404 for unauthorized requests
-            if (response.StatusCode.Equals(HttpStatusCode.Found))
-            {
-                response.StatusCode = HttpStatusCode.NotFound;
-                return response;
-            }
-
-            // Handle the OPTIONS request here
-            if (request.Method.Equals(HttpMethod.Options) &&
-              (response.StatusCode.Equals(HttpStatusCode.MethodNotAllowed) || response.StatusCode.Equals(HttpStatusCode.Unauthorized)))
-            {
-                response.StatusCode = HttpStatusCode.OK;
-            }
-
-            if (!_corsEnabled)
-                return response;
-
-            // Respond only if the client sent an origin header
-            if (request.Headers.Any(header => header.Key.Equals("Origin")))
-            {
-                var originValue = request.Headers.FirstOrDefault(header => header.Key.Equals("Origin")).Value.FirstOrDefault();
-                if (!string.IsNullOrWhiteSpace(originValue))
-                {
-                    var originUri = new Uri(originValue);
-                    var origin = string.Format("{0}://{1}", originUri.Scheme, originUri.Authority);
-                    if (_allowedOrigins.Contains(origin) || _allowedOrigins.Equals("*"))
-                    {
-                        response.Headers.Add("Access-Control-Allow-Origin", origin);
-                        response.Headers.Add("Access-Control-Allow-Methods", Methods);
-                        response.Headers.Add("Access-Control-Allow-Headers", _allowedHeaders);
-                        if (_allowCredentials)
-                            response.Headers.Add("Access-Control-Allow-Credentials", new[] { "true" });
-                    }
-                }
-            }
-            return response;
-        }
-    }
-
-    public class HttpMessageHandler : DelegatingHandler
-    {
-        private readonly Handler _handler;
-
-        public HttpMessageHandler(Handler h)
-        {
-            _handler = h;
-        }
-
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
-                                                                        CancellationToken cancellationToken)
-        {
-            return _handler.Apply(base.SendAsync(request, cancellationToken), request);
-        }
-    }
 }
